7b397a238677aa9e7c56622f6199bc0a9d947111
Rudi Luzar <rudi@rudicode.com>
2014-10-24 13:50:05 -0400

    Developed CRM skeleton, with input loop

--- crm_app.rb
+++ crm_app.rb
@@ -2,5 +2,5 @@ require_relative 'lib/contact'
 require_relative 'lib/rolodex'
 require_relative 'lib/crm'
 
-crm = CRM.new
+CRM.run "Bitmaker CRM"
 
--- lib/crm.rb
+++ lib/crm.rb
@@ -1,7 +1,78 @@
 class CRM
-  def initialize
 
+  def self.run name
+    crm = CRM.new name
+    crm.main_menu
   end
 
+  def initialize name
+    @name = name
+  end
 
+  def main_menu
+    while true
+      display_menu
+      input = get_input
+      process_option input
+      break if input == 7
+    end
+  end
+
+  def display_menu
+    menu = <<CRMMENU
+
+    [1] Add Contact
+    [2] Modify Contact
+    [3] Display All
+    [4] Display Contact
+    [5] Display Attribute
+    [6] Delete Contact
+    [7] Exit
+
+CRMMENU
+    puts menu
+    print "Choose an option: "
+  end
+
+  def get_input
+    gets.chomp().to_i
+  end
+
+  def process_option option
+    # puts "Processing option: #{option}"
+    case option
+    when 1 then add_contact
+    when 2 then modify_contact
+    when 3 then display_all_contacts
+    when 4 then display_contact
+    when 5 then display_attribute
+    when 6 then delete_contact
+    else
+      puts "#{option} is not a valid option."
+    end
+  end
+
+  def add_contact
+    puts "#{self.class}##{__method__} Not implamented yet!!"
+  end
+
+  def modify_contact
+    puts "#{self.class}##{__method__} Not implamented yet!!"
+  end
+
+  def display_all_contacts
+    puts "#{self.class}##{__method__} Not implamented yet!!"
+  end
+
+  def display_contact
+    puts "#{self.class}##{__method__} Not implamented yet!!"
+  end
+
+  def display_attribute
+    puts "#{self.class}##{__method__} Not implamented yet!!"
+  end
+
+  def delete_contact
+    puts "#{self.class}##{__method__} Not implamented yet!!"
+  end
 end
